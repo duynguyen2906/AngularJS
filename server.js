95a63ab2a0cb2514229d1fab247a3666e3938887
duongnhn96 <37268222+duongnhn96@users.noreply.github.com>
2018-05-27 12:45:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+// server.js
 const express = require('express');
-const http = require('http')
-const path = require('path');
-
 const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
 
-app.use(express.static(path.join(__dirname, 'src/app')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'src/app/index.html'));
-});
-
-const port = process.env.PORT || 3000;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
